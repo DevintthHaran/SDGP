11801f69ed39515aeacdccf4bf081006cead7997
Ranud654 <ranudijayasund@gmail.com>
2025-01-29 12:01:20 +0530

    Creating  the feedback form

---
+++ Frontend/sdgp-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Feedback from './components/Feedback'
+
+function App() {
+  return (
+    <div className="w-screen">
+      App
+    <Feedback/>
+    </div>
+  )
+}
+
+export default App

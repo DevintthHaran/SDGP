bb04681d4ce5c76c1029c3412997b59add4b5e19
DevintthHaran <devintth@gmail.com>
2025-03-03 18:39:14 +0530

    style of header overlapping handled

--- Frontend/sdgp-project/src/components/ShopNavbar.jsx
+++ Frontend/sdgp-project/src/components/ShopNavbar.jsx
@@ -2,16 +2,17 @@
 import React from "react";
 import "../style/ShopNavbar.css";
 import 'boxicons/css/boxicons.min.css';
-import '../style/Header.css';
 
 const ShopNavbar = ({ toggleCart }) => {
   return (
+    <div className="shopNarBar">
     <header>
       <div className="nav container">
         <span>Shopping</span>
         <i className="bx bxs-cart" id="cart-icon" onClick={toggleCart}></i>
       </div>
     </header>
+    </div>
   );
 };
 

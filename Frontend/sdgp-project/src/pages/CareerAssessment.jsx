69dc67ac6ad4e4259deae4db9198d4776382ec5d
Ranud654 <138796634+Ranud654@users.noreply.github.com>
2025-03-12 09:41:19 +0530

    Updating the jsx files-part 1

--- Frontend/sdgp-project/src/pages/CareerAssessment.jsx
+++ Frontend/sdgp-project/src/pages/CareerAssessment.jsx
@@ -3,6 +3,7 @@ import Intro from '../components/Intro';
 import Assessment from '../components/Assessment';
 import Results from '../components/Results';
 import '../style/Career.css';
+import Header from '../components/Header';
 
 function CareerAssessment() {
   const [showIntro, setShowIntro] = useState(true);
@@ -27,13 +28,16 @@ function CareerAssessment() {
   };
 
   return (
-    <div >
-      {showIntro && <Intro startAssessment={startAssessment} />}
-      {showAssessment && (
-        <Assessment onComplete={handleAssessmentCompletion} setAnswers={setAnswers} />
-      )}
-      {showResults && <Results restartAssessment={restartAssessment} answers={answers} />}
-     
+    <div>
+      <Header />
+      <div >
+        {showIntro && <Intro startAssessment={startAssessment} />}
+        {showAssessment && (
+          <Assessment onComplete={handleAssessmentCompletion} setAnswers={setAnswers} />
+        )}
+        {showResults && <Results restartAssessment={restartAssessment} answers={answers} />}
+
+      </div>
     </div>
   );
 }

3a877b138c812012e28c464c47525f3c5951cabd
DevintthHaran <devintth@gmail.com>
2025-03-19 22:50:17 +0530

    Feature/haranpages added schdule meeting backend to fetch in mongodb (#20)
    * Update Meeting Style to be user-friendly
    
    * Model to fetch counselor data from  mongoDB
    
    * Interface used to fetch specific data (link and name)
    
    * created services to get approved counselor's data
    
    * Created endpoint for speific positioned counselors
    
    * Schedule Meeting frontend with backend intregation
    
    * Protected and Hide API Key by making it environment variable

---
+++ Backend/SDGP/src/main/java/com/devintth/sdgp/services/CounselorService.java
@@ -0,0 +1,20 @@
+package com.devintth.sdgp.services;
+import com.devintth.sdgp.model.Counselor;
+import com.devintth.sdgp.repository.CounselorProjection;
+import com.devintth.sdgp.repository.CounselorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.List;
+
+@Service
+public class CounselorService {
+
+    @Autowired
+    private CounselorRepository counselorRepository;
+
+    public List<CounselorProjection> getApprovedCounselorsByPosition(String position) {
+        return counselorRepository.findByPositionAndStatus(position, "approved");
+    }
+}
+

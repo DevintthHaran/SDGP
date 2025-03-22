4b6ec9a7147a42fd5235eb2c548ceae0787058a2
DevintthHaran <devintth@gmail.com>
2025-03-23 01:04:00 +0530

    Edited chat css to fit the simualtion

--- Frontend/sdgp-project/src/components/Header.jsx
+++ Frontend/sdgp-project/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <li><a href="/shop">Shop</a></li>
         <li><a href="/meeting">Counseling</a></li>
         <li><a href="/skill">Skill Assessment</a></li>
-        <li><a href="/chat">Interview Simulation</a></li>
+        <li><a href="/simulation">Interview Simulation</a></li>
         <li><a href="/job">Job Market Trend</a></li>
         <li><a href="/career">Career Report</a></li>
         <li><a href="/profile">Profile</a></li>

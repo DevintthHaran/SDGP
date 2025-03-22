58336709cc3108c9673ce347dd02c262b8be63f8
Deni-22 <gadenennscia@gmail.com>
2025-03-22 19:29:21 +0530

    login
    login frontend and backend modify

---
+++ Backend/SDGP/src/main/java/com/devintth/sdgp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.devintth.sdgp.repository;
+
+
+import com.devintth.sdgp.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+
+    User findByUserEmailIdAndUserPassword(String userEmailId, String userPassword);
+}
